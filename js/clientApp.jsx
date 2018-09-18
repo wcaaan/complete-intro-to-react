74c905ebe766c1a4d0b5aa1b7819d48cbbd5e8c7
wcan <wcaaan@gmail.com>
2018-09-18 23:00:55 +0500

    initial commit

---
+++ js/clientApp.jsx
@@ -0,0 +1,18 @@
+const ce = React.createElement;
+
+const MyTitle = function(props) {
+  return ce('div', 'null', ce('h1', { style: { color: props.color } }, props.title));
+};
+
+const MyFirstComponent = function() {
+  return ce(
+    'div',
+    { id: 'first-element' },
+    ce(MyTitle, { title: 'game of thrones', color: '#660000' }),
+    ce(MyTitle, { title: 'how i met your mother', color: '#660000' }),
+    ce(MyTitle, { title: 'silicon valley', color: '#660000' }),
+    ce(MyTitle, { title: 'the big bang theory', color: '#660000' })
+  );
+};
+
+ReactDOM.render(ce(MyFirstComponent), document.getElementById('app'));
