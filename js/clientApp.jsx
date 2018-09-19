24e26350993ad949ead3e1ecff92cab86bedba43
wcan <wcaaan@gmail.com>
2018-09-19 13:14:00 +0500

    devServer config added to webpack.config.js

--- js/clientApp.jsx
+++ js/clientApp.jsx
@@ -1,21 +1,14 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
-const ce = React.createElement;
+const App = () => (
+  <div className="app">
+    <div className="landing">
+      <h1>DaMotionPicture</h1>
+      <input type="text" placeholder="Search" />
+      <a>Or Browser All</a>
+    </div>
+  </div>
+);
 
-const MyTitle = function(props) {
-  return ce('div', 'null', ce('h1', { style: { color: props.color } }, props.title));
-};
-
-const MyFirstComponent = function() {
-  return ce(
-    'div',
-    { id: 'first-element' },
-    ce(MyTitle, { title: 'Game of Thrones', color: '#660000' }),
-    ce(MyTitle, { title: 'How i Met Your Mother', color: '#660000' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: '#660000' }),
-    ce(MyTitle, { title: 'The Black List', color: '#660000' })
-  );
-};
-
-render(ce(MyFirstComponent), document.getElementById('app'));
+render(<App />, document.getElementById("app"));
