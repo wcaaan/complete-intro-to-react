13cf41bc29eff08fcbc6cd4f85e17c912110743e
wcan <wcaaan@gmail.com>
2018-09-23 02:52:03 +0500

    hot module replacment integrated

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -5,7 +5,7 @@ const Landing = () => (
   <div className="landing">
     <h1>DaMotionPicture</h1>
     <input type="text" placeholder="Search" />
-    <Link to='/search'>Or Browser All</Link>
+    <Link to="/search">Or Browser All</Link>
   </div>
 );
 
--- js/clientApp.jsx
+++ js/clientApp.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Landing from './Landing';
-import Search from './Search';
+import App from './App';
 
-const FourOFour = () => <h1>404 Not Found</h1>
+const renderApp = () => {
+  render(<App />, document.getElementById('app'));
+}
+renderApp();
 
-const App = () => (
-  <BrowserRouter>
-    <div className="app">
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route path="/search" component={Search} />
-        <Route component={FourOFour} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
-
-render(<App />, document.getElementById('app'));
+if(module.hot) {
+  module.hot.accept('./App', () => {
+    renderApp();
+  })
+}
